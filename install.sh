d7cbd6502b62858fc03a2678697430cca19b2441
Luis Adolfo Taddei Gonzalez <luisadolfotaddeigonzalez@gmail.com>
2024-08-06 20:03:35 -0700

    first commit

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Update and Upgrade packages
+sudo apt -y update && sudo apt -y upgrade;
+
+# Check and install some needed programs
+for program in stow curl fuse; do
+  if ! command -v $program &> /dev/null; then
+    sudo apt -y install $program
+  fi
+done
+
+# Symlink the dotfiles
+stow nvim
+stow tmux
+stow zsh
+
+# Install zsh and oh-my-posh
+if ! command -v zsh &> /dev/null; then
+  sudo apt -y install zsh
+  sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+# Install neovim
+if ! command -v nvim &> /dev/null; then
+  curl -L -O https://github.com/neovim/neovim/releases/download/v0.10.1/nvim.appimage
+  sudo chmod u+x nvim.appimage
+  sudo mv nvim.appimage /usr/bin/nvim
+else
+  sudo rm /usr/bin/nvim
+  sudo rm -rf ~/.config/nvim/
+fi
+
+# Install tmux
+if ! command -v tmux &> /dev/null; then
+  sudo apt -y install tmux
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  tmux source ~/.tmux.conf
+fi
+
+# Install lazygit
+if ! command -v lazygit &> /dev/null; then
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit /usr/local/bin
+fi
+
+# Sourcing scripts
+for files in shell/*
+do
+  fullpath=$(realpath $file)
+  echo "source $fullpath" >> ~/.zshrc
+done
---
+++ shell/aliases.sh
@@ -0,0 +1,26 @@
+# ALIASES -------------------
+alias ll="ls -l"
+alias lla="ls -la"
+alias cls="clear"
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
+alias rm='rm -l --preserve-root'
+alias ports='sudo netstat -tulanp'
+
+alias nv="nvim"
+alias vim='nvim'
+alias vi='nvim'
+
+alias g='git'
+alias gs="git status"
+alias gl='git log'
+alias ga="git add"
+alias gal='git add .'
+alias gpo='git push -u origin'
+
+alias d="docker"
+alias dc="docker compose"
+alias dl='sudo docker ps -l -q'
+alias dps="sudo docker ps"
+alias di='sudo docker images'
