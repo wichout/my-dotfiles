82c0ceb6aa15764b48814b60f64960aa5074f9b7
Luis Adolfo Taddei Gonzalez <luisadolfotaddeigonzalez@gmail.com>
2025-02-16 12:50:01 -0700

    Changed .zshrc
    Now should be less verbose

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Update and Upgrade packages
-sudo apt -y update && sudo apt -y upgrade;
+sudo apt -y update && sudo apt -y upgrade
 
-mkdir $HOME/.config/
+mkdir $HOME/.config $HOME/.local/bin $HOME/.local/env
 
 # Check and install some needed programs
-for program in stow curl fuse fzf bat keychain; do
-  if ! command -v $program &> /dev/null; then
+for program in stow curl fzf bat keychain; do
+  if ! command -v $program &>/dev/null; then
+    echo "Installing {$program} ..."
     sudo apt -y install $program
+    clear
   fi
 done
 
@@ -16,59 +18,77 @@ done
 stow $HOME/dotfiles/tmux
 stow $HOME/dotfiles/zsh
 
+# Sourcing scripts
+for files in shell/*; do
+  fullpath=$(realpath $file)
+  echo "source $fullpath" >>~/.zshrc
+done
+
+# Install Git
+if ! command -v git &>/dev/null; then
+  echo '--> Installing Git ...'
+
+  sudo apt -y install git-all
+
+  git config --global user.name "wichout"
+  git config --global user.email "luisadolfotaddeigonzalezs@gmail.com"
+
+  clear
+else
+  git config --global user.name "wichout"
+  git config --global user.email "luisadolfotaddeigonzalezs@gmail.com"
+fi
 
 # Install neovim
-if ! command -v nvim &> /dev/null; then
-  echo '---> Installing Neovim...'
-  
-  curl -L -O https://github.com/neovim/neovim/releases/download/v0.10.4/nvim.appimage
-  sudo chmod u+x nvim.appimage
-  sudo mv nvim.appimage /usr/bin/nvim
+if ! command -v nvim &>/dev/null; then
+  echo '--> Installing Neovim ...'
 
-  git clone https://github.com/wichout/neovim-dotfiles.git $HOME/dotfiles/nvim
+  curl -O https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz
+  tar xzvf nvim-linux-x86_64.tar.gz
+  mv nvim-linux-x86_64/bin/nvim $HOME/.local/bin/
+
+  git clone https://github.com/wichout/neovim-dotfiles.git $HOME/dotfiles/nvim/.config/nvim
   stow $HOME/dotfiles/nvim
-  
+
   clear
 fi
 
 # Install tmux
-if ! command -v tmux &> /dev/null; then
+if ! command -v tmux &>/dev/null; then
   echo '---> Installing Tmux'
-  
+
   sudo apt -y install tmux
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-  tmux source ~/.tmux.conf
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+  tmux source $HOME/.tmux.conf
 
   # Plugins
-  mkdir -p ~/.config/tmux/plugins/catppuccin
-  git clone https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+  mkdir -p $HOME/.config/tmux/plugins/catppuccin
+  git clone https://github.com/catppuccin/tmux.git $HOME/.config/tmux/plugins/catppuccin/tmux
+
+  clear
+fi
+
+if ! command -v fdfind &>/dev/null; then
+  echo "--> Installing fd"
+
+  sudo apt install -y fd-find
+  ln -s $(which fdfind) $HOME/.local/bin/fd
 
   clear
 fi
 
 # Install uv
-if ! command -v uv &> /dev/null; then
-  echo '---> Installing uv'
+if ! command -v uv &>/dev/null; then
+  echo '--> Installing uv'
 
   curl -LsSf https://astral.sh/uv/install.sh | sh
 
   clear
 fi
 
-# Install Git
-if ! command -v git &> /dev/null; then
-  echo '---> Installing Git'
-  
-  sudo apt -y install git-all
-  git config --global user.name "wichout"
-  git config --global user.email "luisadolfotaddeigonzalezs@gmail.com"
-  
-  clear
-fi
-
 # Install lazygit
-if ! command -v lazygit &> /dev/null; then
-  echo '---> Installing LazyGit'
+if ! command -v lazygit &>/dev/null; then
+  echo '--> Installing LazyGit'
 
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -79,32 +99,29 @@ if ! command -v lazygit &> /dev/null; then
 fi
 
 # Install Fast Node Manager
-if ! command -v fnm &> /dev/null; then
-  echo '---> Installing Fast Node Manager'
+if ! command -v fnm &>/dev/null; then
+  echo '--> Installing Fast Node Manager'
   curl -fsSL https://fnm.vercel.app/install | bash
   eval "$(fnm env --use-on-cd --shell zsh)"
   clear
 fi
 
-
 # Install zsh and oh-my-zsh
-if ! command -v zsh &> /dev/null; then
+if ! command -v zsh &>/dev/null; then
   echo 'Installing Oh My Zsh and Sourcing files...'
 
   sudo apt -y install zsh
   sudo chsh -s $(which zsh)
 
   # Sourcing scripts
-  for files in shell/*
-  do
+  for files in shell/*; do
     fullpath=$(realpath $file)
-    echo "source $fullpath" >> ~/.zshrc
+    echo "source $fullpath" >>~/.zshrc
   done
-  
+
   sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
   clear
 fi
-
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -4,45 +4,30 @@
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time oh-my-zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
+# Setting PATH
+for dir in "$HOME/bin" "$HOME/.local/bin"; do
+	[ -d "$dir" ] && PATH="$dir:$PATH"
+done
+
+# Loading Env variables if exists
+[ -f "$HOME/.local/bin/env" ] && . "$HOME/.local/bin/env"
+
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="robbyrussell"
 
 # Uncomment one of the following lines to change the auto-update behavior
-# zstyle ':omz:update' mode disabled  # disable automatic updates
-# zstyle ':omz:update' mode auto      # update automatically without asking
-zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+zstyle ':omz:update' mode reminder # just remind me to update when it's time
 
 # Uncomment the following line to change how often to auto-update (in days).
-zstyle ':omz:update' frequency 14
+zstyle ':omz:update' frequency 13
 
 # Uncomment the following line if pasting URLs and other text is messed up.
 # DISABLE_MAGIC_FUNCTIONS="true"
 
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
 # You can set one of the optional three formats:
 # "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-HIST_STAMPS="dd/mm/yyyy"
+HIST_STAMPS="dd.mm.yyyy"
 
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(
 	git
 	zsh-autosuggestions
@@ -51,26 +36,37 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
 # You may need to manually set your language environment
-# export LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='nvim'
+	export EDITOR='vim'
 else
-  export EDITOR='nvim'
+	export EDITOR='mvim'
 fi
 
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
+# Add github ssh keys to the ssh-agent
+eval $(keychain --quiet --eval --agents ssh id_ed25519)
 
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
+# Support for uv
+eval "$(uv generate-shell-completion zsh)"
+eval "$(uvx --generate-shell-completion zsh)"
 
-# eval `keychain --quiet --eval --agents ssh id_ed25519`
+# Adding PNPM
+export PNPM_HOME="/home/wichout/.local/share/pnpm"
+case ":$PATH:" in
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# fnm (Fast Node.js Manager)
+FNM_PATH="/home/wichout/.local/share/fnm"
+if [ -d "$FNM_PATH" ]; then
+	export PATH="/home/wichout/.local/share/fnm:$PATH"
+	eval "$(fnm env)"
+fi
+
+eval "$(fnm env --use-on-cd --shell zsh)"
