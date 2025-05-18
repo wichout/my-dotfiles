6c0767840f2273667475693c2f0fe941c61d5952
wichout <luisadolfotaddeigonzalez@gmail.com>
2025-05-18 16:20:21 -0700

    Sometimes you just have to learn

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ sudo apt -y update && sudo apt -y upgrade
 mkdir $HOME/.config $HOME/.local/bin $HOME/.local/env
 
 # Installing some nedeed programs
-for program in stow curl fzf bat keychain; do
+for program in stow curl fzf bat keychain bat exa xclip ripgrep; do
   if ! command -v $program &>/dev/null; then
-    echo "---> Installing $program ..."
+    echo "-> Installing $program ..."
 
     sudo apt -y install $program
     clear
@@ -23,12 +23,14 @@ done
 stow tmux
 stow zsh
 
+if ! command -v bat &>/dev/null; then
+  ln -s /usr/bin/batcat ~/.local/bin/bat
+fi
+
 # Installing Git
 if ! command -v git &>/dev/null; then
-  echo '--> Installing Git ...'
-
+  echo '-> Installing Git ...'
   sudo apt -y install git-all
-
   clear
 fi
 
@@ -37,86 +39,69 @@ git config --global user.email "luisadolfotaddeigonzalezs@gmail.com"
 
 # Install neovim
 if ! command -v nvim &>/dev/null; then
-  echo '--> Installing Neovim ...'
-
+  echo '-> Installing Neovim ...'
   curl -O https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.tar.gz
   tar xzvf nvim-linux-x86_64.tar.gz
   mv nvim-linux-x86_64/bin/nvim $HOME/.local/bin/
   rm -rf nvim-linux-x86_64.tar.gz nvim-linux-x86_64
-
   git clone https://github.com/wichout/neovim-dotfiles.git $HOME/dotfiles/nvim/.config/nvim
   stow nvim
-
   clear
 fi
 
 # Installing tmux
 if ! command -v tmux &>/dev/null; then
-  echo '---> Installing Tmux ...'
-
+  echo '-> Installing Tmux ...'
   sudo apt -y install tmux
   git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
   tmux source $HOME/.tmux.conf
-
   mkdir -p $HOME/.config/tmux/plugins/catppuccin
   git clone https://github.com/catppuccin/tmux.git $HOME/.config/tmux/plugins/catppuccin/tmux
-
   clear
 fi
 
 # Installing fd
 if ! command -v fdfind &>/dev/null; then
-  echo "--> Installing fd ..."
-
+  echo "-> Installing fd ..."
   sudo apt install -y fd-find
   ln -s $(which fdfind) $HOME/.local/bin/fd
-
   clear
 fi
 
 # Installing uv
 if ! command -v uv &>/dev/null; then
-  echo '--> Installing UV ...'
-
+  echo '-> Installing UV ...'
   curl -LsSf https://astral.sh/uv/install.sh | sh
-
   clear
 fi
 
 # Install lazygit
 if ! command -v lazygit &>/dev/null; then
-  echo '--> Installing LazyGit ...'
-
+  echo '-> Installing LazyGit ...'
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
   tar xf lazygit.tar.gz lazygit
   sudo install lazygit /usr/local/bin
   rm lazygit lazygit.tar.gz
-
   clear
 fi
 
 # Install Fast Node Manager
 if ! command -v fnm &>/dev/null; then
   echo '--> Installing Fast Node Manager ...'
-
   curl -fsSL https://fnm.vercel.app/install | bash
   export PATH="$HOME/.fnm:$PATH"
   eval "$(fnm env --use-on-cd --shell zsh)"
-
   clear
 fi
 
 # Install zsh and oh-my-zsh
 if ! command -v zsh &>/dev/null; then
   echo 'Installing Oh My Zsh and Sourcing files...'
-
   sudo apt -y install zsh
-
   if [ $SHELL != $(which zsh) ]; then
     chsh -s $(which zsh)
   fi
-
   # Sourcing scripts
   for file in shell/*; do
     fullpath=$(realpath "$file")
@@ -124,10 +109,10 @@ if ! command -v zsh &>/dev/null; then
       echo "source $fullpath" >> "$HOME/.zshrc"
     fi
   done
-
   sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+  rm $HOME/.zshrc
+  mv $HOME/.zshrc.pre-oh-my-zsh ~/.zshrc
   clear
 fi
--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,6 +1,9 @@
 # ALIASES -------------------
-alias ll="ls -l"
-alias lla="ls -la"
+alias ls="exa --icons"
+alias ll="exa -lh --icons"
+alias la="exa -lah --icons"
+alias lt="exa -tree --icons"
+
 alias cls="clear"
 alias mv='mv -i'
 alias cp='cp -i'
@@ -24,3 +27,7 @@ alias dc="docker compose"
 alias dl='sudo docker ps -l -q'
 alias dps="sudo docker ps"
 alias di='sudo docker images'
+
+if command -v bat &>/dev/null; then
+    alias cat="bat"
+fi
